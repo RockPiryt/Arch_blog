5428b3acd431ba13149923db38954d1c736b7ab1
RockPiryt <rockpiryt@gmail.com>
2023-06-02 18:03:56 +0200

    'RockPiryt' -create new class Post, add new templates, add data about architecture, add images, update CSS
    New class Post with 2 methods (get_all_post and get_single_post).
    Add new templates for header, footer, contact page and about page.
    Add new link to new API with data about architecture.
    Add relevant images to posts.
    Update style.css (Bootstrap)
    Add scripts.js.

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 from flask import Flask, render_template
-import requests
 from post import Post
 
-#get post from API
-# blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-# response = requests.get(blog_url)
-# all_posts2 = response.json()#list with posts
-
-#single line version to get post from API
-all_posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-
-#create list with post object
-post_list_object = []
-for post in all_posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-    post_list_object.append(post_obj)
-
-#create Flask app
+#Create Flask app
 app = Flask(__name__)
 
 #Routing
 @app.route('/')
-def home():
-    return render_template("index.html", all_posts_html=post_list_object)
+def get_all_posts():
+    post = Post()#create instance with API Link
+    all_posts = post.get_all_post()
+    return render_template("index.html", all_post_html=all_posts)
 
-@app.route('/post/<int:index>')
-def show_post(index):
-    single_post = None
-    for blog_post in post_list_object:
-        if blog_post.id == index:
-            single_post= blog_post
-    return render_template("post.html", post_html=single_post)
+@app.route('/post/<int:post_id>')
+def single_post(post_id):
+    post = Post()
+    single_post = post.get_single_post(post_id)
+    return render_template("post.html", single_post_html=single_post)
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+#Run app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="localhost", port="5000")
--- post.py
+++ post.py
@@ -1,6 +1,15 @@
-class Post:
-    def __init__(self, post_id, title, subtitle, body):
-        self.id = post_id
-        self.title = title
-        self.subtitle = subtitle
-        self.body = body
+import requests
+
+class Post():
+    def __init__(self):
+        self.post_url = "https://api.npoint.io/6c4f0f978204ff0ae340"
+
+    def get_all_post(self):
+        all_post = requests.get(url=self.post_url).json()
+        return all_post
+    
+    def get_single_post(self, post_id):
+        all_post = requests.get(url=self.post_url).json()
+        single_post = [post for post in all_post if int(post["id"]) == int(post_id)]
+        print(single_post)
+        return single_post[0]
